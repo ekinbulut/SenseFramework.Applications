789e0c5f1d7cedf9c5e615fa6e1ae10fd9a621c2
Ekin Bulut <ekinbulut@gmail.com>
2018-12-10 20:07:14 +0300

    nuspec added

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SenseFramework.Applications")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SenseFramework.Applications is a module which provides interfaces to integrate applications to Senseframework domain.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ekinbulut.com")]
 [assembly: AssemblyProduct("SenseFramework.Applications")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("ekinbulut.com")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
